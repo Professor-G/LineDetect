e365b4e4b1d97e929a22f21698feb2fed641e868
Daniel Godines <danielgodinez123@yahoo.com>
2023-04-02 16:02:46 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,18 +6,17 @@ Created on Sat Apr 1 13:46:47 2023
 """
 from setuptools import setup, find_packages, Extension
 
-
 setup(
     name="LineDetect",
     version="0.1",
     author="Asif Abbas & Daniel Godines",
     author_email="danielgodinez123@gmail.com",
-    description="A pipeline for detecting MgII absorption lines",
+    description="A pipeline for detecting absorption lines in spectra",
     license='GPL-3.0',
     url = "https://github.com/Professor-G/LineDetect",
     classifiers=[
 		'Development Status :: 5 - Production/Stable',
-		'Intended Audience :: Developers',
+		'Intended Audience :: Research',
 		'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
 		'Programming Language :: Python :: 3',	   
@@ -25,6 +24,6 @@ setup(
     packages=find_packages('.'),
     install_requires = ['numpy', 'progress', 'astropy', 'scipy', 'matplotlib', 'pandas']
     python_requires='>=3.7,<4',
-    include_package_data=True,
+    include_package_data=False,
     test_suite="nose.collector",
 )
